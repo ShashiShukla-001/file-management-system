dce4179658130155c2b5d25fc7c40994af4d9b6c
ShashiShukla-001 <shuklashashi2001@gmail.com>
2024-06-15 11:21:13 +0530

    Restrict Routes and Logout Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css';
+import React from 'react';
 import { Route,Routes } from 'react-router-dom'
+import { useDispatch } from 'react-redux';
 
 import {Login,Register,HomePage,DashboardPage} from "./pages"
+import { checkIsLoggedIn } from './redux/actionCreators/authActionCreator';
 
 
 const App = () => {
+
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(checkIsLoggedIn());
+  },[])
 
   return (
    <div className='App'>
--- src/components/HomePageComponents/Navigation.jsx
+++ src/components/HomePageComponents/Navigation.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import {signOutUser} from '../../redux/actionCreators/authActionCreator';
+
+  
 
  const NavigationComponent = () => {
+
+  const {isAuthenticated, user} = useSelector(state => state.auth);
+  const dispatch = useDispatch();
+
   return (
    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
      
@@ -9,7 +17,30 @@ import { Link } from 'react-router-dom';
      
      <ul className="navbar-nav ms-auto me-5">
 
-        <li className="nav-item mx-2">
+      {
+        isAuthenticated ? (
+        <>
+          <li className="nav-item mx-2">
+            <p className='my-0 mt-1 mx-2'>
+              <span className="text-light">Welcome,</span>  
+              <span className='text-warning'>{user.displayName}</span>
+            </p>
+          </li>
+
+          <li className="nav-item mx-2">
+          <Link to="/dashboard" className="btn btn-sm btn-primary">
+            Dashboard
+          </Link>
+          </li>
+
+        <li className="nav-item">
+          <button className="btn btn-success btn-sm" onClick={() => dispatch(signOutUser())}>
+            logout
+          </button>
+        </li>
+        </>) : (
+          <>
+           <li className="nav-item mx-2">
           <Link to="/login" className="btn btn-sm btn-primary">
             Login
           </Link>
@@ -20,6 +51,9 @@ import { Link } from 'react-router-dom';
             Register
           </Link>
         </li>
+          </>
+        )
+      };
 
      </ul>
 
